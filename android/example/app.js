b685c142265fc169f692ed71b4aac9f43f7e1883
Timan Rebel <timan@trebel.nl>
2014-07-03 15:37:39 +0200

    Added example to app.js

--- android/example/app.js
+++ android/example/app.js
@@ -1,39 +1,10 @@
-// This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
-// to users on how to use it by example.
-
-
-// open a single window
-var win = Ti.UI.createWindow({
-	backgroundColor:'white'
-});
-var label = Ti.UI.createLabel();
-win.add(label);
-win.open();
-
-// TODO: write your module tests here
 var Parse = require('eu.rebelcorp.parse');
-Ti.API.info("module is => " + Parse);
+    Parse.start('<PARSE-APPLICATION-ID>', '<PARSE-CLIENT-ID>');
 
-label.text = Parse.example();
+	// To enable Android Push Notifications
+	Parse.enablePush();
 
-Ti.API.info("module exampleProp is => " + Parse.exampleProp);
-Parse.exampleProp = "This is a test value";
+	// Subscribe of unsubscribe to Parse Channels
+    Parse.subscribeChannel('user_123');
 
-if (Ti.Platform.name == "android") {
-	var proxy = Parse.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
-	});
-
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
-}
-
+    Parse.unsubscribeChannel('user_123');
