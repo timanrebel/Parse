f227d7fed16cdddc3c8b276a7d592bf05fb2029d
Fokke Zandbergen <mail@fokkezb.nl>
2015-03-30 12:12:17 +0200

    Update example
    - Remove deprecated enablePush
    - Remove removed start args
    - Add putValue

--- android/example/app.js
+++ android/example/app.js
@@ -1,10 +1,11 @@
 var Parse = require('eu.rebelcorp.parse');
-    Parse.start('<PARSE-APPLICATION-ID>', '<PARSE-CLIENT-ID>');
 
-	// To enable Android Push Notifications
-	Parse.enablePush();
+Parse.start();
 
-	// Subscribe of unsubscribe to Parse Channels
-    Parse.subscribeChannel('user_123');
+// Subscribe of unsubscribe to Parse Channels
+Parse.subscribeChannel('user_123');
 
-    Parse.unsubscribeChannel('user_123');
+Parse.unsubscribeChannel('user_123');
+
+// Set value on the installation object
+Parse.putValue('foo', 'bar');
